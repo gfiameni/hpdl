66cecad2779880a1eee3754ee57d8f8193944ca5
Giuseppe Fiameni <gfiamen1@dgxslurm.m100.cineca.it>
2022-06-20 10:45:46 +0200

    cudagraph

---
+++ code/ddp_cudagraph.py
@@ -0,0 +1,351 @@
+import torch
+from torch.utils.data.distributed import DistributedSampler
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+import argparse
+import os
+import random
+import numpy as np
+import time
+import torch.distributed as dist
+from torch.distributed.optim import ZeroRedundancyOptimizer
+
+
+def print_peak_memory(prefix, device):
+    if device == 0:
+        print(f"{prefix}: {torch.cuda.max_memory_allocated(device) // 1e6}MB ")
+
+def setup(rank, world_size):
+    os.environ['MASTER_ADDR'] = '127.0.0.1'
+    os.environ['MASTER_PORT'] = '12355'
+
+    # initialize the process group
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
+
+    print('Distr rank ', torch.distributed.get_rank())
+
+
+def cleanup():
+    dist.destroy_process_group()
+
+# Save checkpoint file draft function
+def save_checkpoint(ddp_model, world_size):
+    CHECKPOINT_PATH = tempfile.gettempdir() + "/model.checkpoint"
+    if rank == 0:
+        # All processes should see same parameters as they all start from same
+        # random parameters and gradients are synchronized in backward passes.
+        # Therefore, saving it in one process is sufficient.
+        torch.save(ddp_model.state_dict(), CHECKPOINT_PATH)
+
+    # Use a barrier() to make sure that process 1 loads the model after process
+    # 0 saves it.
+    dist.barrier()
+    # configure map_location properly
+    map_location = {'cuda:%d' % 0: 'cuda:%d' % rank}
+    ddp_model.load_state_dict(
+        torch.load(CHECKPOINT_PATH, map_location=map_location))
+
+    optimizer.zero_grad()
+    outputs = ddp_model(torch.randn(20, 10))
+    labels = torch.randn(20, 5).to(rank)
+    loss_fn = nn.MSELoss()
+    loss_fn(outputs, labels).backward()
+    optimizer.step()
+
+    # Use a barrier() to make sure that all processes have finished reading the
+    # checkpoint
+    dist.barrier()
+
+    if rank == 0:
+        os.remove(CHECKPOINT_PATH)
+
+    cleanup()
+
+
+def set_random_seeds(random_seed=0):
+
+    torch.manual_seed(random_seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(random_seed)
+    random.seed(random_seed)
+
+def evaluate(model, device, test_loader):
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data[0].to(device), data[1].to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    accuracy = correct / total
+    return accuracy
+
+def main():
+
+    num_epochs_default = 1
+    batch_size_default = 256 # 256 # 1024
+    learning_rate_default = 0.1
+    random_seed_default = 0
+    model_dir_default = "saved_models"
+    model_filename_default = "resnet_distributed.pth"
+
+    # Each process runs on 1 GPU device specified by the local_rank argument.
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--local_rank", type=int, help="Local rank. Necessary for using the torch.distributed.launch utility.")
+    parser.add_argument("--num_epochs", type=int, help="Number of training epochs.", default=num_epochs_default)
+    parser.add_argument("--batch_size", type=int, help="Training batch size for one process.", default=batch_size_default)
+    parser.add_argument("--learning_rate", type=float, help="Learning rate.", default=learning_rate_default)
+    parser.add_argument("--random_seed", type=int, help="Random seed.", default=random_seed_default)
+    parser.add_argument("--model_dir", type=str, help="Directory for saving models.", default=model_dir_default)
+    parser.add_argument("--model_filename", type=str, help="Model filename.", default=model_filename_default)
+    parser.add_argument("--resume", action="store_true", help="Resume training from saved checkpoint.")
+    parser.add_argument("--channels-last", action="store_true", help="Channels last")
+    parser.add_argument("--use-zero", action="store_true", help="Zero Redundancy Optimizer")
+    argv = parser.parse_args()
+
+    local_rank = argv.local_rank
+    num_epochs = argv.num_epochs
+    batch_size = argv.batch_size
+    learning_rate = argv.learning_rate
+    random_seed = argv.random_seed
+    model_dir = argv.model_dir
+    model_filename = argv.model_filename
+    resume = argv.resume
+
+    if local_rank is None:
+        local_rank = int(os.environ["LOCAL_RANK"])
+        print('Local rank ', local_rank)
+    
+    # Create directories outside the PyTorch program
+    # Do not create directory here because it is not multiprocess safe
+    '''
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+    '''
+
+    model_filepath = os.path.join(model_dir, model_filename)
+
+    # We need to use seeds to make sure that the models initialized in different processes are the same
+    set_random_seeds(random_seed=random_seed)
+    
+    # setup(local_rank, world_size)
+
+    # Initializes the distributed backend which will take care of sychronizing nodes/GPUs
+    torch.distributed.init_process_group(backend="nccl")
+    # torch.distributed.init_process_group(backend="gloo")
+
+    # Encapsulate the model on the GPU assigned to the current process
+    model = torchvision.models.resnet18(pretrained=False)
+
+    device = torch.device("cuda:{}".format(local_rank))
+
+    # Mixed precision 
+    scaler = torch.cuda.amp.GradScaler()
+
+    if torch.backends.cudnn.version() >= 7603 and argv.channels_last:
+        model = model.to(device, memory_format=torch.channels_last)  # Module parameters need to be channels last
+    else:
+        model = model.to(device)
+
+    if argv.use_zero:
+        print_peak_memory("Max memory allocated after creating local model", local_rank)
+    ddp_model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[local_rank], output_device=local_rank)
+    if argv.use_zero:
+        print_peak_memory("Max memory allocated after creating DDP", local_rank)
+
+    # We only save the model who uses device "cuda:0"
+    # To resume, the device for the saved model would also be "cuda:0"
+    if resume == True:
+        map_location = {"cuda:0": "cuda:{}".format(local_rank)}
+        ddp_model.load_state_dict(torch.load(model_filepath, map_location=map_location))
+
+    # Prepare dataset and dataloader
+    transform = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    # Data should be prefetched
+    # Download should be set to be False, because it is not multiprocess safe
+    train_set = torchvision.datasets.CIFAR10(root="./data/cifar-10", train=True, download=True, transform=transform) 
+    test_set = torchvision.datasets.CIFAR10(root="./data/cifar-10", train=False, download=True, transform=transform)
+
+    # Restricts data loading to a subset of the dataset exclusive to the current process
+    train_sampler = DistributedSampler(dataset=train_set)
+
+    train_loader = DataLoader(dataset=train_set, batch_size=batch_size, sampler=train_sampler, num_workers=4)
+    # Test loader does not have to follow distributed sampling strategy
+    test_loader = DataLoader(dataset=test_set, batch_size=128, shuffle=False, num_workers=4)
+
+    criterion = nn.CrossEntropyLoss()
+
+    if argv.use_zero:
+        optimizer = ZeroRedundancyOptimizer(
+            ddp_model.parameters(),
+            optimizer_class=optim.SGD,
+            lr=learning_rate,
+            momentum=0.9, 
+            weight_decay=1e-5
+        )
+    else:
+        optimizer = optim.SGD(ddp_model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=1e-5)
+
+# CUDAGraph
+"""
+    N, D_in, H, D_out = 640, 4096, 2048, 1024
+    model = torch.nn.Sequential(torch.nn.Linear(D_in, H),
+                                torch.nn.Dropout(p=0.2),
+                                torch.nn.Linear(H, D_out),
+                                torch.nn.Dropout(p=0.1)).cuda()
+    loss_fn = torch.nn.MSELoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+
+    # Placeholders used for capture
+    static_input = torch.randn(80, 3, 32, 32, device='cuda')
+    static_target = torch.randn(N, D_out, device='cuda')
+
+    print(static_input.size())
+
+    s = torch.cuda.Stream()
+    s.wait_stream(torch.cuda.current_stream())
+    with torch.cuda.stream(s):
+        for i in range(3):
+            optimizer.zero_grad(set_to_none=True)
+            y_pred = model(static_input)
+            loss = criterion(y_pred, static_target)
+            loss.backward()
+            optimizer.step()
+    torch.cuda.current_stream().wait_stream(s)
+
+    # capture
+    g = torch.cuda.CUDAGraph()
+    # Sets grads to None before capture, so backward() will create
+    # .grad attributes with allocations from the graph's private pool
+    optimizer.zero_grad(set_to_none=True)
+    with torch.cuda.graph(g):
+        static_y_pred = model(static_input)
+        static_loss = loss_fn(static_y_pred, static_target)
+        static_loss.backward()
+        optimizer.step()
+
+    real_inputs = [torch.rand_like(static_input) for _ in range(10)]
+    real_targets = [torch.rand_like(static_target) for _ in range(10)]
+
+    for data, target in zip(real_inputs, real_targets):
+        # Fills the graph's input memory with new data to compute on
+        static_input.copy_(data)
+        static_target.copy_(target)
+        # replay() includes forward, backward, and step.
+        # You don't even need to call optimizer.zero_grad() between iterations
+        # because the captured backward refills static .grad tensors in place.
+        g.replay()
+        # Params have been updated. static_y_pred, static_loss, and .grad
+        # attributes hold values from computing on this iteration's data.
+"""
+# End of CUDA Graph
+
+
+    s = torch.cuda.Stream()
+    s.wait_stream(torch.cuda.current_stream())
+    with torch.cuda.stream(s):
+        for data in train_loader:
+            if torch.backends.cudnn.version() >= 7603 and argv.channels_last:
+                inputs, labels = data[0].to(device, memory_format=torch.channels_last), data[1].to(device, memory_format=torch.channels_last)
+            else:
+                inputs, labels = data[0].to(device), data[1].to(device)
+
+            print('Inputs ', inputs.size())
+            print('Labels ', labels.size())
+
+            optimizer.zero_grad()
+
+            with torch.cuda.amp.autocast():
+                outputs = ddp_model(inputs)
+                loss = criterion(outputs, labels)
+                
+            scaler.scale(loss).backward()
+            scaler.update()
+    torch.cuda.current_stream().wait_stream(s)       
+
+    # capture
+    g = torch.cuda.CUDAGraph()
+    # Sets grads to None before capture, so backward() will create
+    # .grad attributes with allocations from the graph's private pool
+    optimizer.zero_grad(set_to_none=True)
+    with torch.cuda.graph(g):
+            if torch.backends.cudnn.version() >= 7603 and argv.channels_last:
+                inputs, labels = data[0].to(device, memory_format=torch.channels_last), data[1].to(device, memory_format=torch.channels_last)
+            else:
+                inputs, labels = data[0].to(device), data[1].to(device)
+
+            optimizer.zero_grad()
+            with torch.cuda.amp.autocast():
+                outputs = ddp_model(inputs)
+                loss = criterion(outputs, labels)
+                
+            scaler.scale(loss).backward()
+            scaler.update()
+
+
+    # Loop over the dataset multiple times
+    for epoch in range(num_epochs):
+
+        t0 = time.time()
+        # Save and evaluate model routinely
+        if epoch % 1 == 0:
+            if local_rank == 0:
+                accuracy = evaluate(model=ddp_model, device=device, test_loader=test_loader)
+                torch.save(ddp_model.state_dict(), model_filepath)
+                print("-" * 75)
+                print("Epoch: {}, Accuracy: {}".format(epoch, accuracy))
+                print("-" * 75)
+
+        ddp_model.train()
+        
+        for data in train_loader:
+            if torch.backends.cudnn.version() >= 7603 and argv.channels_last:
+                inputs, labels = data[0].to(device, memory_format=torch.channels_last), data[1].to(device, memory_format=torch.channels_last)
+            else:
+                inputs, labels = data[0].to(device), data[1].to(device)
+
+            print('Inputs ', inputs.size())
+            print('Labels ', labels.size())
+
+            optimizer.zero_grad()
+
+            with torch.cuda.amp.autocast():
+                outputs = ddp_model(inputs)
+                loss = criterion(outputs, labels)
+                
+            scaler.scale(loss).backward()
+
+            """
+            if argv.use_zero:
+                print_peak_memory("Max memory allocated before optimizer step()", local_rank)
+            scaler.step(optimizer)
+            if argv.use_zero:
+                print_peak_memory("Max memory allocated after optimizer step()", local_rank)
+            """
+
+        # Updates the scale for next iteration.
+            scaler.update()
+            
+            #loss.backward()
+            #optimizer.step()
+
+        print("Local Rank: {}, Epoch: {}, Training ...".format(local_rank, epoch))
+        print("Time {} seconds".format(round(time.time() - t0, 2)))
+
+            
+if __name__ == "__main__":
+    main()
