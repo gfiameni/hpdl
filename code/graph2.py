bfe830c258f30183217c874da027cb729a69f263
Giusepp Fiameni <gfiameni@nvidia.com>
2022-06-22 15:27:39 +0000

    graph + amp

--- code/graph2.py
+++ code/graph2.py
@@ -1,6 +1,11 @@
 import torch
 import os
 import time
+import gc
+import torch.cuda.amp as amp
+
+torch.backends.cudnn.benchmark = True
+enable_amp = True
 
 if __name__ == "__main__":
 
@@ -21,6 +26,8 @@ if __name__ == "__main__":
                                 torch.nn.Linear(H, D_out),
                                 torch.nn.Dropout(p=0.1)).cuda()
 
+    model = torch.jit.script(model)
+    gscaler = amp.GradScaler(enabled = enable_amp) 
 
     # Cuda Graphs requires that DDP is captured on a side stream
     # It is important to synchronize the streams after the DDP initialization
@@ -29,7 +36,7 @@ if __name__ == "__main__":
     #s.wait_stream(torch.cuda.current_stream())
     with torch.cuda.stream(s):
             ddp_model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[local_rank], 
-                output_device=local_rank, find_unused_parameters=True)
+                output_device=local_rank)
     torch.cuda.current_stream().wait_stream(s)
 
     loss_fn = torch.nn.CrossEntropyLoss()
@@ -43,50 +50,71 @@ if __name__ == "__main__":
     s.wait_stream(torch.cuda.current_stream())
     with torch.cuda.stream(s):
         output = torch.tensor([rank]).cuda(rank)
-        for i in range(11):
-            optimizer.zero_grad(set_to_none=True)
-            y_pred = ddp_model(static_input)
-            loss = loss_fn(y_pred, static_target)
-            loss = loss
-            loss.backward()
+        for _ in range(11):
+            with amp.autocast(enabled = enable_amp):
+                optimizer.zero_grad(set_to_none=True)
+                y_pred = ddp_model(static_input)
+                loss = loss_fn(y_pred, static_target)
+                loss = loss
+            #loss.backward()
+            gscaler.scale(loss).backward()
             optimizer.step()
-    torch.cuda.current_stream().wait_stream(s)
 
-    # capture
-    g = torch.cuda.CUDAGraph()
-    # Sets grads to None before capture, so backward() will create
-    # .grad attributes with allocations from the graph's private pool
-    optimizer.zero_grad(set_to_none=True)
-    with torch.cuda.graph(g):
-        static_y_pred = ddp_model(static_input)
-        static_loss = loss_fn(static_y_pred, static_target)
-        static_loss.backward()
-        optimizer.step()
+        # wait and clean up
+        s.synchronize()
+        gc.collect()
+        torch.cuda.empty_cache()
 
+        # capture
+        g = torch.cuda.CUDAGraph()
+        # Sets grads to None before capture, so backward() will create
+        # .grad attributes with allocations from the graph's private pool
+        optimizer.zero_grad(set_to_none=True)
+        with torch.cuda.graph(g):
+            with amp.autocast(enabled = enable_amp):
+                static_y_pred = ddp_model(static_input)
+                static_loss = loss_fn(static_y_pred, static_target)
+                #static_loss.backward()
+            gscaler.scale(static_loss).backward()
+            optimizer.step()
+
+            """
+            if local_rank == 0:
+                reqs = []
+                req1 = dist.isend(tensor=static_input, dst=1) # enqueue the operation instead of issuing it
+                reqs.append(req1)
+                reqs.wait_all() # blocking until all operators are finished 
+            if local_rank == 1:
+                # Rank 1: 
+                reqs = []
+                req1 = dist.irecv(tensor=static_input, src=0)
+                reqs.append(req1)
+                reqs.wait_all() 
         """
-        if local_rank == 0:
-            reqs = []
-            req1 = dist.isend(tensor=static_input, dst=1) # enqueue the operation instead of issuing it
-            reqs.append(req1)
-            reqs.wait_all() # blocking until all operators are finished 
-        if local_rank == 1:
-            # Rank 1: 
-            reqs = []
-            req1 = dist.irecv(tensor=static_input, src=0)
-            reqs.append(req1)
-            reqs.wait_all() 
-        """
+        torch.cuda.current_stream().wait_stream(s)
 
     real_inputs = [torch.rand_like(static_input) for _ in range(1000)]
     real_targets = [torch.rand_like(static_target) for _ in range(1000)]
 
+    start = torch.cuda.Event(enable_timing=True)
+    end = torch.cuda.Event(enable_timing=True)
+
+    # Waits for everything to finish running
+    torch.cuda.synchronize()
     # torch.cuda.synchronize()
-    t_time = time.perf_counter_ns()
+    #t_time = time.perf_counter_ns()
+    start.record()
     for data, target in zip(real_inputs, real_targets):
         static_input.copy_(data)
         static_target.copy_(target)
         # replay() includes forward, backward, and step.
         g.replay()
-    t_time = time.perf_counter_ns() - t_time
+    end.record()
+    torch.cuda.synchronize()
+
+    print(f"Elapsed time: {start.elapsed_time(end)*1e-6}")
+
+
+    # t_time = time.perf_counter_ns() - t_time
     # torch.cuda.synchronize()
-    print(f"Execution time: {t_time*1e-6} ms")
+    # print(f"Execution time: {t_time*1e-6} ms")
